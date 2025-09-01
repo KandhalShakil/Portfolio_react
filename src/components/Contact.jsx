66d9cf9ef69fda1274539edc66dcf3c11d4b827a
Kandhal Shakil <148697267+KandhalShakil@users.noreply.github.com>
2025-09-01 18:11:37 +0530

    Update image URL for emailjs template

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -91,7 +91,7 @@ const Contact = () => {
     // In a real application, you would send the OTP to the user's email here
     console.log("Generated OTP:", newOtp);
     emailjs.send("service_emks25r", "template_h8r9vlo", {
-      image : 'pic.jpg',
+      image : 'https://github.com/KandhalShakil/Portfolio_react/blob/main/public/pic.jpg?raw=true',
       passcode: newOtp,
       email: email,
     } , "HiT1qgF3NG4BIwyQY")
