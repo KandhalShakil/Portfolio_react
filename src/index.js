4f53c9f59a44c595fdfccaecfea510c7d25f15fc
KandhalShakil <kandhalshakil@gmail.com>
2025-11-09 00:04:28 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
 import App from './App.jsx';
-=======
-import App from './App';
-import reportWebVitals from './reportWebVitals';
->>>>>>> 3853bce4e82a9ecac5ee5dbaade27d8167a64c80
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,11 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-=======
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> 3853bce4e82a9ecac5ee5dbaade27d8167a64c80
