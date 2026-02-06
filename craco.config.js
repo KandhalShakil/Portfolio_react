a277dc821e8cc791d673db43dd745a8aef1c0094
KandhalShakil <kandhalshakil@gmail.com>
2026-02-06 09:25:05 +0530

    Optimize: Simplify CRACO source map configuration

--- craco.config.js
+++ craco.config.js
@@ -1,32 +1,23 @@
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
+      // Disable source map loader to prevent warnings from dependencies
       if (webpackConfig.module && Array.isArray(webpackConfig.module.rules)) {
         webpackConfig.module.rules = webpackConfig.module.rules.map((rule) => {
-          const usesSourceMapLoader =
+          if (
             rule.enforce === 'pre' &&
             Array.isArray(rule.use) &&
-            rule.use.some(
-              (loaderEntry) =>
-                loaderEntry &&
-                typeof loaderEntry === 'object' &&
-                typeof loaderEntry.loader === 'string' &&
-                loaderEntry.loader.includes('source-map-loader')
-            );
-
-          if (usesSourceMapLoader) {
-            const existingExclude = rule.exclude
-              ? Array.isArray(rule.exclude)
-                ? rule.exclude
-                : [rule.exclude]
-              : [];
-            rule.exclude = existingExclude;
+            rule.use.some((loader) => loader?.loader?.includes('source-map-loader'))
+          ) {
+            // Disable source-map-loader
+            return {
+              ...rule,
+              use: rule.use.filter((loader) => !loader?.loader?.includes('source-map-loader'))
+            };
           }
-
           return rule;
         });
       }
-
       return webpackConfig;
     },
   },
